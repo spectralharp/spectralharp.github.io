60f3024dcc3ad0266cf79b3011b64c99ea1dfa4e
Chao Hsu Lin <chaohsul0226@gmail.com>
2022-02-24 01:29:29 +0800

    [update] about page bio

--- src/components/About.js
+++ src/components/About.js
@@ -1,38 +1,45 @@
 import './About.scss';
-import i18n from '../data/i18n.json';
+import { useContext } from 'react';
+import { LanguageContext } from '../context/language-context';
+import ReactMarkdown from 'react-markdown';
 
-export default function About({ pageLang }) {
+export default function About() {
+
+  const language = useContext(LanguageContext);
+
   return (
     <section className="about container">
       <h1 className="title">
-        {i18n[pageLang].loc.about} : //
+        {language.loc.about} : //
       </h1>
-      <img
-        className='about__profile'
-        src="/images/profile.png"
-        alt="Headshot"
-        data-aos="fade-up"
-        data-aos-delay="50"
-      />
-      <h2
+      <div className='info-box'>
+        <div className='about__profile'>
+          <img
+            className='about__pfp'
+            src="/images/profile.jpg"
+            alt="Headshot"
+            data-aos="zoom-in"
+            data-aos-delay="50"
+            width={200}
+            height={200}
+         />
+        </div>
+      </div>
+      <h2 className='about__flavor-item'>{language.about.name}</h2>
+      <h3
         className="title"
         data-aos="fade-up"
         data-aos-delay="100"
       >
         Web Developer / Game Developer / Artist
-      </h2>
-      <p
+      </h3>
+      <div
         data-aos="fade-up"
         data-aos-delay="150"
       >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer suscipit
-        dignissim nulla quis volutpat. Nunc pretium tellus lacus, sed tempus felis
-        iaculis et. Sed in metus massa. Pellentesque ac hendrerit ligula. Maecenas
-        et pellentesque mauris, at vestibulum dui. Nam dapibus egestas tortor, vel
-        auctor lacus porta eu. Suspendisse at nisl sit amet quam dignissim gravida.
-        Duis ac est dictum, gravida sapien vel, tincidunt massa. Fusce placerat
-        tincidunt finibus. Nam volutpat vehicula iaculis.
-      </p>
+        <ReactMarkdown children={language.about.bio} />
+
+      </div>
     </section>
   );
 }
